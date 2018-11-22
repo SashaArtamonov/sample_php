b475cab26a53ca55c05cfe1f8c1f6b47ac3980d8
Oleksandr Artamonov <Oleksandr_Artamonov@epam.com>
2018-11-22 17:17:28 +0200

    update top3

--- ajax/ajax.top.php
+++ ajax/ajax.top.php
@@ -7,25 +7,29 @@ $qGet2 = $mysqli->query('
 ');
 while ($dGet = $qGet2->fetch_object())
 $rowsmax[] = $dGet;
+
+$transdate = date('m-d-Y', time());
+$month = date('m');
+
 ?>
 
-<p style="left:200px;font-weight:bold;">Top 3 November</p>
-<div class="topfaces">
-    <img src="<?php echo htmlspecialchars($rowsmax[0]->path); ?>" class="topphotos" alt="<?php echo htmlspecialchars($rowsmax[0]->name); ?>" data-token="<?php echo $rowsmax[0]->token; ?>" data-score="<?php echo $rowsmax[0]->score; ?>" width="150" height="200" />
+<p style="left:200px;font-weight:bold;">Top 3 <?php $month?></p>
+<div class="faces">
+    <img src="<?php echo htmlspecialchars($rowsmax[0]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rowsmax[0]->name); ?>" data-token="<?php echo $rowsmax[0]->token; ?>" data-score="<?php echo $rowsmax[0]->score; ?>" width="150" height="200" />
     <ul>
         <li><?php echo htmlspecialchars($rowsmax[0]->name); ?></li>
         <li>Score: <?php echo number_format($rowsmax[0]->score, 0, ',', ' '); ?></li>
     </ul>
 </div>
-<div class="topfaces">
-    <img src="<?php echo htmlspecialchars($rowsmax[1]->path); ?>" class="topphotos" alt="<?php echo htmlspecialchars($rowsmax[1]->path); ?>" data-token="<?php echo $rowsmax[1]->token; ?>" data-score="<?php echo $rowsmax[1]->score; ?>" width="150" height="200" />
+<div class="faces">
+    <img src="<?php echo htmlspecialchars($rowsmax[1]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rowsmax[1]->path); ?>" data-token="<?php echo $rowsmax[1]->token; ?>" data-score="<?php echo $rowsmax[1]->score; ?>" width="150" height="200" />
     <ul>
         <li><?php echo htmlspecialchars($rowsmax[1]->name); ?></li>
         <li>Score: <?php echo number_format($rowsmax[1]->score, 0, ',', ' '); ?></li>
     </ul>
 </div>
-<div class="topfaces">
-    <img src="<?php echo htmlspecialchars($rowsmax[2]->path); ?>" class="topphotos" alt="<?php echo htmlspecialchars($rowsmax[2]->path); ?>" data-token="<?php echo $rowsmax[2]->token; ?>" data-score="<?php echo $rowsmax[2]->score; ?>" width="150" height="200" />
+<div class="faces">
+    <img src="<?php echo htmlspecialchars($rowsmax[2]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rowsmax[2]->path); ?>" data-token="<?php echo $rowsmax[2]->token; ?>" data-score="<?php echo $rowsmax[2]->score; ?>" width="150" height="200" />
     <ul>
         <li><?php echo htmlspecialchars($rowsmax[2]->name); ?></li>
         <li>Score: <?php echo number_format($rowsmax[2]->score, 0, ',', ' '); ?></li>
