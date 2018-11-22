6537495fdb0d6949ab64d40acbcd37e739da4a1c
Oleksandr Artamonov <Oleksandr_Artamonov@epam.com>
2018-11-22 16:24:49 +0200

    update main htmls

--- all_top.php
+++ all_top.php
@@ -15,9 +15,9 @@ require_once('process.php');
     </head>
     <body id="element">
         <header>
-            <h1><center>&#1050;&#1086;&#1088;&#1086;&#1083;&#1077;&#1074;&#1072; EPAM</center></h1>
+           <h1><center>&#1050;&#1086;&#1088;&#1086;&#1083;&#1077;&#1074;&#1072; EPAM</center></h1>
            <a href="index.php">&#1043;&#1086;&#1083;&#1086;&#1074;&#1085;&#1072;</a>
-           <a href="all_members.php">&#1056;&#1077;&#1081;&#1090;&#1080;&#1085;&#1075;</a>
+           <a href="all_top.php">&#1056;&#1077;&#1081;&#1090;&#1080;&#1085;&#1075;</a>
         </header>  
         <div id="dual">
             <?php require_once('ajax/ajax.top.php'); ?>
