701c8ce9b28eb2e146c0fd17450a17e5d85fdada
Oleksandr Artamonov <Oleksandr_Artamonov@epam.com>
2018-11-22 14:35:36 +0200

    edit logic a bit

--- ajax/ajax.dual.php
+++ ajax/ajax.dual.php
@@ -3,9 +3,9 @@ $qGet = $mysqli->query('
         SELECT token, name, path, score
         FROM photos
         ORDER BY RAND()
-        LIMIT 8');
+        LIMIT 2');
 while ($dGet = $qGet->fetch_object())
-    $rows[] = $dGet;
+$rows[] = $dGet;
 
 $qGet2 = $mysqli->query('
         SELECT *
@@ -19,7 +19,7 @@ while ($dGet = $qGet2->fetch_object())
 
 
 <div class="faces">
-    <img src="<?php echo htmlspecialchars($rows[0]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rows[0]->name); ?>" data-token="<?php echo $rows[0]->token; ?>" data-score="<?php echo $rows[0]->score; ?>"   />
+    <img src="<?php echo htmlspecialchars($rows[0]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rows[0]->name); ?>" data-token="<?php echo $rows[0]->token; ?>" data-score="<?php echo $rows[0]->score; ?>" width="266" height="400" />
     <ul>
         <li><?php echo htmlspecialchars($rows[0]->name); ?></li>
         <li>Score: <?php echo number_format($rows[0]->score, 0, ',', ' '); ?></li>
@@ -28,7 +28,7 @@ while ($dGet = $qGet2->fetch_object())
 <p id="middle">OR</p>
 <img src="loader3.gif" alt="Chargement..." width="64" height="64" id="loader" />
 <div class="faces">
-    <img src="<?php echo htmlspecialchars($rows[1]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rows[1]->path); ?>" data-token="<?php echo $rows[1]->token; ?>" data-score="<?php echo $rows[1]->score; ?>"  />
+    <img src="<?php echo htmlspecialchars($rows[1]->path); ?>" class="photos" alt="<?php echo htmlspecialchars($rows[1]->path); ?>" data-token="<?php echo $rows[1]->token; ?>" data-score="<?php echo $rows[1]->score; ?>" width="266" height="400" />
     <ul>
         <li><?php echo htmlspecialchars($rows[1]->name); ?></li>
         <li>Score: <?php echo number_format($rows[1]->score, 0, ',', ' '); ?></li>
